fe300515bdd182451580f436c5975dedb3bdd8ab
rafael garcia <rago89@gmail.com>
2021-08-10 09:18:57 +0200

    add about me section

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -1,4 +1,5 @@
 import { showcase } from "../../layout/showcase.js";
+import { aboutMe } from "../../layout/about-me.js";
 
 /**
  * The home page.
@@ -10,6 +11,7 @@ export const home = () => {
   const bodySection = document.createElement("div");
   bodySection.className = "body-section";
   bodySection.appendChild(showcase());
+  bodySection.appendChild(aboutMe());
 
   return bodySection;
 };
